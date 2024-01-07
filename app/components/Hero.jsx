615ccf69e7b454d05ae26c889b8c6ba9fde83cf8
abdulahaddf <abdulahad.df1@gmail.com>
2024-01-07 23:23:28 +0600

    hero section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 import img from "../assets/pic.jpg"
 export default function Hero() {
+  
   return (
     <div className='w-1/2 mx-auto'>
       <Image placeholder="blur" src={img} alt='fg'/>
